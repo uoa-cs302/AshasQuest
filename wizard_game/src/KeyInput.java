de5b1dc7daba5ecce906b0a9396a6cdb02f598b7
Hassaan Mirza <hasf1998@gmail.com>
2019-04-08 16:32:06 -0700

    2019/09/04 additions

---
+++ wizard_game/src/KeyInput.java
@@ -0,0 +1,59 @@
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class KeyInput extends KeyAdapter {
+
+    private Handler handler;
+
+    public KeyInput(Handler handler) {
+        this.handler = handler;
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int key = e.getKeyCode();
+
+        for (int i = 0; i < handler.object.size(); i++) {
+            GameObject tempObject = handler.object.get(i);
+
+            if (tempObject.getID() == ID.Player) {
+                if (key == KeyEvent.VK_W) {
+                    handler.setUp(true);
+                }
+                if (key == KeyEvent.VK_S) {
+                    handler.setDown(true);
+                }
+                if (key == KeyEvent.VK_A) {
+                    handler.setLeft(true);
+                }
+                if (key == KeyEvent.VK_D) {
+                    handler.setRight(true);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int key = e.getKeyCode();
+
+        for (int i = 0; i < handler.object.size(); i++) {
+            GameObject tempObject = handler.object.get(i);
+
+            if (tempObject.getID() == ID.Player) {
+                if (key == KeyEvent.VK_W) {
+                    handler.setUp(false);
+                }
+                if (key == KeyEvent.VK_S) {
+                    handler.setDown(false);
+                }
+                if (key == KeyEvent.VK_A) {
+                    handler.setLeft(false);
+                }
+                if (key == KeyEvent.VK_D) {
+                    handler.setRight(false);
+                }
+            }
+        }
+    }
+}
