4506964ca78a11c702c71b48a0365a2740d4a924
Hassaan Mirza <hasf1998@gmail.com>
2019-04-21 07:25:31 -0700

    Removed Launcher class, deemed it unnecessary, moved main method to Game class. Runs same as before

---
+++ 21_4_2019_AshasQuest/AshasQuest/src/RockTile.java
@@ -0,0 +1,23 @@
+public class RockTile extends Tile {
+
+
+
+    public RockTile(int id) {
+
+        super(Assets.stone, id);
+
+    }
+
+
+
+    @Override
+
+    public boolean isSolid(){
+
+        return true;
+
+    }
+
+
+
+}
