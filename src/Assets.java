b4bf96937a4689c7cc2bc364c1b05691b4273e66
Kimberley Evans-Parker <kim@evans-parker.com>
2019-04-24 16:00:19 +1200

    BOSS_TERRAIN HashMap

--- src/Assets.java
+++ src/Assets.java
@@ -1,143 +1,112 @@
 import java.awt.Font;
-
 import java.awt.image.BufferedImage;
-
-
 
 public class Assets {
     //This class is where we create our assets(static entities, creature entities, pop ups etc), load them with their
     //textures (from a spritesheet or an image). For the movable characters, like the player and the enemies, we need
     //several such images that will be in an array together.
 
-
-
     private static final int width = 32, height = 32;
 
-
-
     public static Font font28;
-
     //Here, we declare all of our assets that need images.
 
-
     public static BufferedImage dirt, grass, stone, tree, rock;
-
     public static BufferedImage wood;
-
     public static BufferedImage[] player_down, player_up, player_left, player_right;
-
     public static BufferedImage[] zombie_down, zombie_up, zombie_left, zombie_right;
-
     public static BufferedImage[] btn_start;
-
     public static BufferedImage inventoryScreen;
+    public static final HashMap<String, BufferedImage> BOSS_TERRAIN = new HashMap<>();
 
+    public static void boss(){
+        SpriteSheet sheet = new SpriteSheet(ImageLoader.loadImage("../res/boss.png"), 32, 32);
+        BOSS_TERRAIN.put("MAT_TOP_LEFT_CORNER", sheet.crop(2, 3, 1, 1));
+        BOSS_TERRAIN.put("MAT_TOP_EDGE", sheet.crop(2, 4, 1, 1));
+        BOSS_TERRAIN.put("MAT_TOP_RIGHT_CORNER", sheet.crop(2, 5, 1, 1));
+        BOSS_TERRAIN.put("MAT_LEFT_EDGE", sheet.crop(3, 3, 1, 1));
+        BOSS_TERRAIN.put("MAT", sheet.crop(3, 4, 1, 1));
+        BOSS_TERRAIN.put("MAT_RIGHT_EDGE", sheet.crop(3, 5, 1, 1));
+        BOSS_TERRAIN.put("MAT_BOTTOM_LEFT_CORNER", sheet.crop(4, 3, 1, 1));
+        BOSS_TERRAIN.put("MAT_BOTTOM_EDGE", sheet.crop(4, 4, 1, 1));
+        BOSS_TERRAIN.put("MAT_BOTTOM_RIGHT_CORNER", sheet.crop(4, 5, 1, 1));
 
+        BOSS_TERRAIN.put("TOP_LEFT_CORNER", sheet.crop(0, 0, 1, 1));
+        BOSS_TERRAIN.put("TOP_EDGE", sheet.crop(0, 1, 1, 1));
+        BOSS_TERRAIN.put("TOP_RIGHT_CORNER", sheet.crop(0, 2, 1, 1));
+        BOSS_TERRAIN.put("LEFT_EDGE", sheet.crop(1, 0, 1, 1));
+        BOSS_TERRAIN.put("FLOOR", sheet.crop(1, 1, 1, 1));
+        BOSS_TERRAIN.put("RIGHT_EDGE", sheet.crop(1, 2, 1, 1));
+        BOSS_TERRAIN.put("BOTTOM_LEFT_CORNER", sheet.crop(2, 0, 1, 1));
+        BOSS_TERRAIN.put("BOTTOM_EDGE", sheet.crop(2, 1, 1, 1));
+        BOSS_TERRAIN.put("BOTTOM_RIGHT_CORNER", sheet.crop(2, 2, 1, 1));
+        
+        BOSS_TERRAIN.put("TOP_LEFT_INSIDE", sheet.crop(1, 4, 1, 1));
+        BOSS_TERRAIN.put("TOP_RIGHT_INSIDE", sheet.crop(1, 3, 1, 1));
+        BOSS_TERRAIN.put("BOTTOM_LEFT_INSIDE", sheet.crop(0, 4, 1, 1));
+        BOSS_TERRAIN.put("BOTTOM_RIGHT_INSIDE", sheet.crop(0, 3, 1, 1));
+
+        BOSS_TERRAIN.put("BLACK", sheet.crop(3, 6, 1, 1));
+    }
 
     public static void init(){
 
+        boss();
+
         //We initialise all those previously declared assets, and begin to load them with their images
-
         font28 = new Font("arial",Font.BOLD,28);
-
-
 
         //The spritesheet contains almost all of the images of the assets, so it makes sense to just load sub images
         //from the main image.
         SpriteSheet sheet = new SpriteSheet(ImageLoader.loadImage("../res/sheet.png"));
 
-
-
         inventoryScreen = ImageLoader.loadImage("../res/inventoryScreen.png");
-
-
 
         wood = sheet.crop(width, height, width, height);
 
-
-
         btn_start = new BufferedImage[2];
-
         btn_start[0] = sheet.crop(width * 6, height * 4, width * 2, height);
-
         btn_start[1] = sheet.crop(width * 6, height * 5, width * 2, height);
-
 
         //Each direction of movement requires an array to describe it,
         //because each motion requires a "left and a right" motion
-
         player_down = new BufferedImage[2];
-
         player_up = new BufferedImage[2];
-
         player_left = new BufferedImage[2];
-
         player_right = new BufferedImage[2];
-
 
         //the width*4 or width*3 etc are relative to what grid space in the sprite sheet that particular image is.
         //eg if something is in the first column of the sheet, it would be at x=0,y=0, while if it was in the second grid
         //space, next to the first, with the same y value, it would be at x=width, y=0. If it was in the grid space next
         //to that, it would be width*2, y=0.
-
         player_down[0] = sheet.crop(width * 4, 0, width, height);
-
         player_down[1] = sheet.crop(width * 5, 0, width, height);
-
         player_up[0] = sheet.crop(width * 6, 0, width, height);
-
         player_up[1] = sheet.crop(width * 7, 0, width, height);
-
         player_right[0] = sheet.crop(width * 4, height, width, height);
-
         player_right[1] = sheet.crop(width * 5, height, width, height);
-
         player_left[0] = sheet.crop(width * 6, height, width, height);
-
         player_left[1] = sheet.crop(width * 7, height, width, height);
 
-
-
         zombie_down = new BufferedImage[2];
-
         zombie_up = new BufferedImage[2];
-
         zombie_left = new BufferedImage[2];
-
         zombie_right = new BufferedImage[2];
 
-
-
         zombie_down[0] = sheet.crop(width * 4, height * 2, width, height);
-
         zombie_down[1] = sheet.crop(width * 5, height * 2, width, height);
-
         zombie_up[0] = sheet.crop(width * 6, height * 2, width, height);
-
         zombie_up[1] = sheet.crop(width * 7, height * 2, width, height);
-
         zombie_right[0] = sheet.crop(width * 4, height * 3, width, height);
-
         zombie_right[1] = sheet.crop(width * 5, height * 3, width, height);
-
         zombie_left[0] = sheet.crop(width * 6, height * 3, width, height);
-
         zombie_left[1] = sheet.crop(width * 7, height * 3, width, height);
 
-
-
         dirt = sheet.crop(width, 0, width, height);
-
         grass = sheet.crop(width * 2, 0, width, height);
-
         stone = sheet.crop(width * 3, 0, width, height);
-
         tree = sheet.crop(0, 0, width, height * 2);
-
         rock = sheet.crop(0, height * 2, width, height);
-
     }
-
-
 
 }
--- src/World.java
+++ src/World.java
@@ -1,243 +1,131 @@
 import java.awt.Graphics;
 
-
 public class World {
 
-
     //widht and height will be used in terms of times of the world.
-
     private Handler handler;
-
     private int width, height;
-
     private int spawnX, spawnY;
-
     //this will hold the ids of tiles and what position they will be created in. There will be rows of tiles
     //and columns of tiles
     private int[][] tiles;
-
     //Entities
-
     private EntityManager entityManager;
-
     // Item
-
     private ItemManager itemManager;
 
-
-
     public World(Handler handler, String path){
-
         this.handler = handler;
-
         entityManager = new EntityManager(handler, new Player(handler, 100, 100));
-
         itemManager = new ItemManager(handler);
-
         // Temporary entity code!
-
         entityManager.addEntity(new Tree(handler, 132, 250));
-
         entityManager.addEntity(new Rock(handler, 132, 450));
-
         entityManager.addEntity(new Rock(handler, 350, 300));
-
         entityManager.addEntity(new Rock(handler, 400, 345));
-
         entityManager.addEntity(new Tree(handler, 625, 325));
-
-
 
         loadWorld(path);
 
-
         //This sets where on the map the player character will show up.
-
         entityManager.getPlayer().setX(spawnX);
-
         entityManager.getPlayer().setY(spawnY);
-
     }
-
-
 
     public void tick(){
-
         itemManager.tick();
-
         entityManager.tick();
-
     }
 
-
-
     public void render(Graphics g){
-
         //Below is ensuring the boundaries of what is rendered on the map, keeping them within the tile limits
         //and the camera limits.
-
         int xStart = (int) Math.max(0, handler.getGameCamera().getxOffset() / Tile.TILEWIDTH);
-
         int xEnd = (int) Math.min(width, (handler.getGameCamera().getxOffset() + handler.getWidth()) / Tile.TILEWIDTH + 1);
-
         int yStart = (int) Math.max(0, handler.getGameCamera().getyOffset() / Tile.TILEHEIGHT);
-
         int yEnd = (int) Math.min(height, (handler.getGameCamera().getyOffset() + handler.getHeight()) / Tile.TILEHEIGHT + 1);
-
-
 
 
         //Loop extends from beginning of map to end of map
         for(int y = yStart;y < yEnd;y++){
-
             for(int x = xStart;x < xEnd;x++){
-
                 //Here, we are converting the tile co-ordinates to pixels. x by width, y by height.
                 getTile(x, y).render(g, (int) (x * Tile.TILEWIDTH - handler.getGameCamera().getxOffset()),
-
                         (int) (y * Tile.TILEHEIGHT - handler.getGameCamera().getyOffset()));
-
             }
-
         }
-
         // Items
-
         itemManager.render(g);
-
         //Entities
-
         entityManager.render(g);
-
     }
-
-
 
     public Tile getTile(int x, int y){
-
         //gets x and y positions for the tile. It is going to look for the id in the tiles array at x,y.
-
         //if
         if(x < 0 || y < 0 || x >= width || y >= height)
-
             return Tile.grassTile;
 
-
         //accesses tiles array in Tile class.
-
         Tile t = Tile.tiles[tiles[x][y]];
-
         //if th tile that we got is equal to nothing, we run a default of a dirtTile.
         if(t == null)
-
             return Tile.dirtTile;
-
         return t;
-
     }
 
-
-
     private void loadWorld(String path){
-
         //This loads the world file(s) that we have in our res, and grabs tokens from it to use for indexing.
         String file = Utils.loadFileAsString(path);
-
         //We must split up each individual number in the file around every amount of white space.
         String[] tokens = file.split("\\s+");
-
         //The first two numbers at the very top of the world file tell what the world's width and height is.
 
-
         width = Utils.parseInt(tokens[0]);
-
         height = Utils.parseInt(tokens[1]);
-
         //the second and third will decide where on the world the player will spawn at. What tile will the player spawn at.
-
         spawnX = Utils.parseInt(tokens[2]);
-
         spawnY = Utils.parseInt(tokens[3]);
-
         //After that comes the world data. The tile data will be represented by an ID number on that position on that
         //file.Ensure that all of the ids are relative to the height and width you have set.
 
-
-
         tiles = new int[width][height];
-
         //This loops through every element of our tiles array and set the tiles in it equal to something
         for(int y = 0;y < height;y++){
-
             for(int x = 0;x < width;x++){
-
                 //We are converting the x and y of the 4 loops into the 1 dimensional array index.
                 //We are adding 4 because the first 4 numbers are not actual world data.
-
                 tiles[x][y] = Utils.parseInt(tokens[(x + y * width) + 4]);
-
             }
-
         }
-
     }
-
-
 
     public int getWidth(){
-
         return width;
-
     }
-
-
 
     public int getHeight(){
-
         return height;
-
     }
-
-
 
     public EntityManager getEntityManager() {
-
         return entityManager;
-
     }
-
-
 
     public Handler getHandler() {
-
         return handler;
-
     }
-
-
 
     public void setHandler(Handler handler) {
-
         this.handler = handler;
-
     }
-
-
 
     public ItemManager getItemManager() {
-
         return itemManager;
-
     }
-
-
 
     public void setItemManager(ItemManager itemManager) {
-
         this.itemManager = itemManager;
-
     }
-
-
 
 }
