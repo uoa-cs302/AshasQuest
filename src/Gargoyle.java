cfb6b1f155fb86403c036691e6e6892c1fb3e691
Kimberley Evans-Parker <kim@evans-parker.com>
2019-04-25 22:46:35 +1200

    Gargoyle drops rock in correct pos and can't be walked over

--- src/Gargoyle.java
+++ src/Gargoyle.java
@@ -6,9 +6,9 @@ public class Gargoyle extends StaticEntity {
         super(handler, x, y, Tile.TILEWIDTH * 2, Tile.TILEHEIGHT * 2);
 
         bounds.x = 0;
-        bounds.y = (int) (height * 2 / 5f);
+        bounds.y = (int) (height - height / 1.5f);
         bounds.width = width;
-        bounds.height = (int) (height - height / 1.5f);
+        bounds.height = (int) (height / 1.5f);
     }
 
     @Override
@@ -19,7 +19,7 @@ public class Gargoyle extends StaticEntity {
     @Override
     public void die(){
         //The below line indicates that if this static entity dies, to drop the item that the player can pick up
-        handler.getWorld().getItemManager().addItem(Item.rockItem.createNew((int) x, (int) y));
+        handler.getWorld().getItemManager().addItem(Item.rockItem.createNew((int) x + width / 2, (int) y + height / 2));
     }
 
     @Override
