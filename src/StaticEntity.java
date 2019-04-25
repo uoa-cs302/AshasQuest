11076d8ce62732f7b6769a4d160047f2f717d271
Kimberley Evans-Parker <kim@evans-parker.com>
2019-04-25 17:16:31 +1200

    removed old sprites

--- src/StaticEntity.java
+++ src/StaticEntity.java
@@ -1,13 +1,7 @@
 public abstract class StaticEntity extends Entity {
 
-
-
     public StaticEntity(Handler handler, float x, float y, int width, int height){
-
         super(handler, x, y, width, height);
-
     }
-
-
 
 }
