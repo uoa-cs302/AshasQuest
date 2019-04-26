137b6824097578d3adda69eab422b2da383f2bdc
mmir415 <41189871+mmir415@users.noreply.github.com>
2019-04-26 15:26:56 +1200

    Edited Game
    Ensured that the timer remains paused when the inventory screen is active

---
+++ src/Enemy.java
@@ -0,0 +1,22 @@
+import java.awt.*;
+
+public class Enemy extends Creature {
+    public Enemy(Handler handler, float x, float y, int width, int height) {
+        super(handler, x, y, width, height);
+    }
+
+    @Override
+    public void tick() {
+
+    }
+
+    @Override
+    public void render(Graphics g) {
+
+    }
+
+    @Override
+    public void die() {
+
+    }
+}
