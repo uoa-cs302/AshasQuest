0f808c8daecfb4a35b4ba6abc5f58fd0384a66fe
Kimberley Evans-Parker <kim@evans-parker.com>
2019-04-27 11:51:28 +1200

    removed extra print statement

--- src/ImageLoader.java
+++ src/ImageLoader.java
@@ -1,38 +1,18 @@
 import java.awt.image.BufferedImage;
-
 import java.io.IOException;
-
-
-
 import javax.imageio.ImageIO;
 import java.io.File;
 
-
-
 public class ImageLoader {
 
-
-
     public static BufferedImage loadImage(String path){
-
         try {
-            System.out.println(path);
-            return ImageIO.read(new File(path)); //this should be as stream right um yes
-                                                                              // umm, apparently no? maybe?
-
-
+            return ImageIO.read(new File(path));
         } catch (IOException e) {
-
             e.printStackTrace();
-
             System.exit(1);
-
         }
-
         return null;
-
     }
-
-
 
 }
